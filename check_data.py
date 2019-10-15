fixed normalization

check_data.py
@@ -24,16 +24,21 @@ targets_dir = os.path.join(root_dir,'targets')
 list_ = os.listdir(word_audio_npy_dir)
 
 name = random.choice(list_)[:-4]
-audio_npy = np.load(os.path.join(word_audio_npy_dir,name) + '.npy')
+
+audio_npy_scaled = np.load(os.path.join(word_audio_npy_dir,name) + '.npy')
+
+# NOTE: it is very important to use np.int16, otherwise the audio is completely
+# altered!
+audio_npy_scaled_back = np.int16(audio_npy_scaled*2**15)
+
 original = AudioSegment.from_mp3(os.path.join(word_audio_dir,name) + '.mp3')
 Fs = original.frame_rate
 
-wavf.write('test.wav',Fs,audio_npy)
+wavf.write('test.mp3',Fs,audio_npy_scaled_back)
 
 targets_dir = os.path.join(root_dir,'targets')
 
 target = np.load(os.path.join(targets_dir,name) + '.npy')
-
 str_ = ''
 
 for nbr in target :    

prepare_data.py
@@ -47,7 +47,10 @@ for i,kana in enumerate(kana_list):
         if os.stat(audio_path).st_size != 0 :
             audio = AudioSegment.from_file(audio_path, format='mp3')
             audio_npy = np.array(audio.get_array_of_samples())
-            np.save(os.path.join(word_audio_npy_dir,name),audio_npy)
+            audio_npy_scaled = audio_npy/2**15
+            
+            # If the audio file is 16-bit, the range is -2^15 2^15
+            np.save(os.path.join(word_audio_npy_dir,name),audio_npy_scaled)
 
             target = []
             for char in chars:
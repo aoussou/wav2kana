6fdde8ec4ea2af07b3386dfbb743640a700b16d0
john <aoussou@talisol.com>
2019-11-28 00:59:44 +0900

    simplifid processing file

--- prepare_kore_word.py
+++ prepare_kore_word.py
@@ -17,15 +17,22 @@ def create_dir(path):
     if not os.path.isdir(path):
         os.makedirs(path)
 ###############################################################################  
+
+csv_file_name = 'kore-words.csv'
+word_audio_file_folder_name =  'kore-sound-vocab-munged'
+
+csv_file_name = 'kore-sentences.csv'
+word_audio_file_folder_name =  'kore-sound-sentences-munged'
+
 root_dir = '../../data/audio/kore'
-csv_file_name = 'Optimized Kore - Sheet1.csv'
+
 csv_file_path = os.path.join(root_dir,csv_file_name)
 df = pd.read_csv(csv_file_path)
-kana_list = df['Vocab-kana']
+kana_list = df['kana']
 
 correspondence_dict = json.load(open('./correspondence.json'))
 
-word_audio_file_folder_name =  'kore-sound-vocab-munged'
+
 word_audio_dir = os.path.join(root_dir,word_audio_file_folder_name)
 word_audio_npy_dir = os.path.join(root_dir,'word_audio_npy') 
 create_dir(word_audio_npy_dir)
@@ -33,49 +40,41 @@ create_dir(word_audio_npy_dir)
 targets_dir = os.path.join(root_dir,'targets')
 create_dir(targets_dir)
 
-names_list = df['Vocab-sound-local']
+names_list = df['file_name']
 
 n_max_audio = 0
 n_max_target = 0
 
 for i,kana in enumerate(kana_list):
+        
+    chars = list(kana)
+    name = names_list[i]
     
-    # Some audio files are missing, skip them.
-    if type(names_list[i]) == str:
-        chars = list(kana)
-        name = names_list[i][7:-5]
-        
-        audio_path = os.path.join(word_audio_dir,name) + '.mp3'
-        
-        if os.stat(audio_path).st_size != 0 :
-            audio = AudioSegment.from_file(audio_path, format='mp3')
-            audio_npy = np.array(audio.get_array_of_samples())
-            audio_npy_scaled = audio_npy/2**15
+    audio_path = os.path.join(word_audio_dir,name) + '.mp3'
+    audio = AudioSegment.from_file(audio_path, format='mp3')
+    audio_npy = np.array(audio.get_array_of_samples())
+    audio_npy_scaled = audio_npy/2**15
+    
+    # If the audio file is 16-bit, the range is -2^15 2^15
+    np.save(os.path.join(word_audio_npy_dir,name),audio_npy_scaled)
+
+    if len(audio_npy_scaled) > n_max_audio:
+        n_max_audio = len(audio_npy_scaled)
+
+    target = []
+    for char in chars:
+        target.append(correspondence_dict[char])
+          
             
-            # If the audio file is 16-bit, the range is -2^15 2^15
-            np.save(os.path.join(word_audio_npy_dir,name),audio_npy_scaled)
+    target = np.array(target)
+    np.save(os.path.join(targets_dir,name),target)
 
-            if len(audio_npy_scaled) > n_max_audio:
-                n_max_audio = len(audio_npy_scaled)
-
-            target = []
-            for char in chars:
-        
-                if char == '・':                        
-                        break
-                else:
-                    target.append(correspondence_dict[char])
-                  
-                    
-            target = np.array(target)
-            np.save(os.path.join(targets_dir,name),target)
-        
-            if len(target) > n_max_target:
-                n_max_target = len(target)
+    if len(target) > n_max_target:
+        n_max_target = len(target)
                 
-            
-        
-            print(i,kana,target)
+    print(i,kana,target)
+
+
          
 print('The longest audio npy length is',n_max_audio)
 print('The longest audio npy length is',n_max_target)
